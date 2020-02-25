891f3021c9ed2d63c3fa0c169a41e0fea015f1ff
apancernaja <pancernaja@icloud.com>
2020-02-25 20:24:56 +0200

    Commit first attempt

---
+++ test/src/task1.java
@@ -0,0 +1,17 @@
+import java.util.Random;
+
+public class task1 {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int rand_int1 = random.nextInt(10);
+        int rand_int2 = random.nextInt(10);
+        int rand_int3 = random.nextInt(10);
+
+        System.out.println("First number "+rand_int1);
+        System.out.println("First number "+rand_int2);
+        System.out.println("First number "+rand_int3);
+
+
+
+    }
+    }
