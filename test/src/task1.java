ce7e6b58a90c81312e73a24181e4a1ae9a09fe4b
apancernaja <pancernaja@icloud.com>
2020-02-25 20:30:53 +0200

    Commit - task 1 finished

--- test/src/task1.java
+++ test/src/task1.java
@@ -7,9 +7,12 @@ public class task1 {
         int rand_int2 = random.nextInt(10);
         int rand_int3 = random.nextInt(10);
 
-        System.out.println("First number "+rand_int1);
-        System.out.println("First number "+rand_int2);
-        System.out.println("First number "+rand_int3);
+        int sum = rand_int1 + rand_int2 + rand_int3;
+        System.out.println("First number" + rand_int1);
+        System.out.println("Second number" + rand_int2);
+        System.out.println("Third number" + rand_int3);
+
+        System.out.println("Sum = " + sum);
 
 
 
